15b025e2ed078c18b9437ed7a07f69332a3fcbf4
lmiralles0 <60629914+lmiralles0@users.noreply.github.com>
2022-05-23 18:02:20 -0300

    Add files via upload

---
+++ Ejercicios_2/Bibliotecas_Ejercicios/Class1.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Bibliotecas_Ejercicios
+{
+    public class Validador
+    {
+        public static bool Validar (int valor, int min, int max)
+        {
+            if(valor >= min && valor <= max && max > min)
+                return true;
+            else
+                return false;
+        }
+    }
+}
---
+++ Ejercicios_2/ConsoleQ/Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using Bibliotecas_Ejercicios;
+
+namespace ConsoleQ
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Title = "Ejercicio 1";
+            
+            double acumulador = 0;
+            double numMax = int.MinValue;
+            double numMin = int.MaxValue;
+
+            for (int i = 0; i < 10; i++)
+            {
+                Console.Write("Ingrese un Nº entre 100 y -100: ");
+                int numero = int.Parse((string)Console.ReadLine());
+                if (Validador.Validar(numero, -100, 100))
+                {
+                    acumulador += numero;
+                    if (numero > numMax)
+                    {
+                        numMax = numero;
+                    }
+                    else if (numero < numMin)
+                    {
+                        numMin = numero;
+                    }
+                }
+                else
+                {
+                    Console.WriteLine("Error al ingresar Nº: ");
+                    i--;  
+                }
+            }
+            Console.WriteLine("El menor Nº es {0}, el mayor Nº es {1}, el promedio es {2}", numMin, numMax, acumulador / 10);
+            Console.ReadKey();
+            
+        }
+    }
+}
