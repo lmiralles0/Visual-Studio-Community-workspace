5b63a2e02c465abaafd091827697c2e99c3842c4
lmiralles0 <60629914+lmiralles0@users.noreply.github.com>
2022-09-12 22:22:38 -0300

    Add files via upload

---
+++ Ejercicio_Colecciones/FormL/Form1.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace FormL
+{
+    public partial class Form1 : Form
+    {
+        private List<string> lista;
+        private Dictionary<string, int> dicccionario;
+        
+        public Form1()
+        {
+            InitializeComponent();
+            lista = new List<string>();
+            dicccionario = new Dictionary<string, int>();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Palabras(textBox1.Text);
+
+            MessageBox.Show();
+        }
+
+        public void Palabras(string aux)
+        {
+            
+        }
+    }
+}
