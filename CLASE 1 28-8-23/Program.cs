d95061725c0a2acc20de482d73aed82cd75ded9d
lmiralles0 <lucianomiralles96@gmail.com>
2023-08-28 22:46:47 -0300

    shitw

--- CLASE 1 28-8-23/Program.cs
+++ CLASE 1 28-8-23/Program.cs
@@ -4,8 +4,55 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
-            Console.ReadLine();
+            int valorIngresado;
+            string buffer;
+            bool estado;
+            int contador = 0;
+            int acumuladorIngreso = 0;
+            float promedio;
+            int minimo = int.MaxValue;
+            int maximo = int.MinValue;
+
+            do
+            {
+                Console.WriteLine("Ingrese numero");
+                buffer = Console.ReadLine();
+                estado = int.TryParse(buffer, out valorIngresado);
+                
+                while(!estado)
+                {
+                    Console.WriteLine("Error ingrese un numero...");
+                    buffer = Console.ReadLine();
+                    estado = int.TryParse(buffer, out valorIngresado);
+
+                }
+
+                acumuladorIngreso += valorIngresado;
+                
+                
+                if(valorIngresado > maximo)
+                {
+                    maximo = valorIngresado;
+                }
+                if (valorIngresado < minimo)
+                {
+                    minimo = valorIngresado;
+                }
+
+                contador++;
+
+            } while(contador < 5);
+
+            if (contador !=  0) 
+            {
+                promedio = (float)acumuladorIngreso / contador;
+                Console.WriteLine("El promedio es {0} ", promedio.ToString());
+            }
+
+            Console.WriteLine("MAXIMO: {0} \nMINIMO: {1}", maximo.ToString(), minimo.ToString());
+            Console.ReadKey();
+
+
         }
     }
 }
