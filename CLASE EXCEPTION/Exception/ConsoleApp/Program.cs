456e9533ed68b980b05e63e082148353b28e61a4
lmiralles0 <lucianomiralles96@gmail.com>
2023-11-27 02:54:34 -0300

    as

---
+++ CLASE EXCEPTION/Exception/ConsoleApp/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+         
+            
+
+            try
+            {
+                int num1 = 0;
+                int num = 5 / num1;
+                Console.WriteLine(num.ToString());
+            }
+            catch(Exception)
+            {
+                Console.WriteLine("No se puede dividir por 0 capo");
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ CLASE GENERICS/Generics/ConsoleApp/Program.cs
@@ -0,0 +1,14 @@
+﻿
+namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Lista<short> lista = new Lista<short>();
+
+            
+
+        }
+    }
+}
