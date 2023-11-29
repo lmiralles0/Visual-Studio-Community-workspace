253fc152fa4a6bee0b0949b1d542e719e54386d2
MCvitani <MCvitani@P1-PCLM>
2023-11-29 14:14:48 -0300

    as

---
+++ CLASE ARCHIVOS/Archivos/ConsoleApp/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ConsoleApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(Environment.GetFolderPath(Environment.SpecialFolder.CommonProgramFiles));
+            string path = $"{ Environment.GetFolderPath(Environment.SpecialFolder.Desktop)}";
+            path += @"\Directorio\";
+
+            Console.WriteLine(path);
+            Console.WriteLine(Directory.Exists(path));
+
+            if(!Directory.Exists(path) ) 
+            {
+                Directory.CreateDirectory(path);
+            }
+            Console.ReadKey();
+            
+        }
+    }
+}
